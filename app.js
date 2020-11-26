94883d14ae805ba534c2e583df1fda043dbc0a67
Allen-TaiLin <skygary2000@gmail.com>
2020-11-26 12:25:04 +0800

    feat: add morgan and setting

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 // app.js
 const express = require('express')
+const exphbs = require('express-handlebars')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 自定義時間格式 (token)
+morgan.token('localDate', function getDate(req) {
+  let date = new Date()
+  return date.toLocaleString()
+})
+
+// 定義format
+morgan.format('messageLog', ':localDate | :method from :url')
+// 使用自定義的format
+app.use(morgan('messageLog'))
+
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  const message = '列出全部 Todo'
+  res.render('index', { message })
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  const message = '新增 Todo 頁面'
+  res.render('new', { message })
 })
 
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  const message = '顯示一筆 Todo'
+  res.render('detail', { message })
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  const message = '新增一筆  Todo'
+  res.render('detail', { message })
 })
 
 app.listen(port, () => {
